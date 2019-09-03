45b03308652f8e25cb99308c4721074e9266ef0a
Rohan Phillips <rohan@solutions-insurance.com>
2019-09-03 15:08:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   configure do
   	set :views, "app/views"
-  	set :public_dir, "public"
+  	#set :public_dir, "public"
   end
 
   get "/" do
